d4448c073b656cb3454999619d9b148296b9e1bb
Fajri Fath <vipercode305@gmail.com>
2023-03-08 23:51:26 +0700

    Configuration for export image for logo.

---
+++ public/images/logo/index.tsx
@@ -0,0 +1,8 @@
+import designGraphicLogo from "./design-grafis.svg";
+import htmlLogo from "./htmlcss 1.svg";
+import mysqlLogo from "./mysql.svg";
+import laravelLogo from "./laravel.svg";
+import phpLogo from "./php.png";
+import pythonLogo from "./python.svg";
+
+export { designGraphicLogo, htmlLogo, mysqlLogo, laravelLogo, phpLogo, pythonLogo };
