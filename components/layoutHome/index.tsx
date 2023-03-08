f7667c67a324d9662aaa4cc7ebf05c59e3acbcf6
Fajri Fath <vipercode305@gmail.com>
2023-03-08 23:48:19 +0700

    Configuration for export function.

---
+++ components/layoutHome/index.tsx
@@ -0,0 +1,7 @@
+import Header from "@/components/layoutHome/header";
+import Navigation from "@/components/layoutHome/navigation";
+import ClassLists from "@/components/layoutHome/classLists";
+import AboutUs from "@/components/layoutHome/aboutUs";
+import Footer from "@/components/layoutHome/footer";
+
+export { Header, Navigation, ClassLists, AboutUs, Footer };
