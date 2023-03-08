1b5f652cc4344910b5a630c13760a0bbf50bcd51
Fajri Fath <vipercode305@gmail.com>
2023-03-08 23:42:07 +0700

    Setting for next js html tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,24 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+
+config.autoAddCss = false;
+import "yet-another-react-lightbox/styles.css";
+import "yet-another-react-lightbox/plugins/captions.css";
+import type { AppProps } from "next/app";
+import { useEffect } from "react";
+import { SSRProvider } from "@react-aria/ssr";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    require("bootstrap/dist/js/bootstrap.bundle.min.js");
+  });
+  return (
+    <>
+      <SSRProvider>
+        <Component {...pageProps} />
+      </SSRProvider>
+    </>
+  );
 }
